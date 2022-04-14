e0cb701f4b3457946de65bf37d1393164a876944
shawon-majid <shawon.majid@gmail.com>
2022-04-15 03:46:40 +0600

    learned stage, scene, layout

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,18 +2,15 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import view.ViewManager;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			ViewManager manager = new ViewManager();
+			primaryStage = manager.getMainStage();
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
